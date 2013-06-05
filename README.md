9e25ef3567df347cc5c6b9194f4ffb4d907d8f29
George Lee <mclee@foolme.net>
2013-06-05 16:47:54 +0800

    integrate vim-vundle and dotfiles

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ##Mac environment setup
 
 This is the place where i put my standard setup environment. Use it at your own risk.
+
+to install:
+
+		curl -L http://raw.github.com/mclee/mac_setup/master/install.sh | sh
--- install.sh
+++ install.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
 # install homebrew
-
 ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+
+# install the formulas
 KEGS=`curl -L https://raw.github.com/mclee/mac_setup/master/kegs`
 for KEG in $KEGS; do
 	brew install $KEG
 done
+
+# install dotfiles
+curl -L https://raw.github.com/mclee/dotfiles/master/install.sh | sh
+
+# install vim
+curl -L http://raw.github.com/mclee/vim-vundle/master/install.sh | sh
